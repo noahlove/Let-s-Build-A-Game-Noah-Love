3f2f9e42a3f5fa9933cc3d94a2eb47f24d3d4626
noahlove2658 <noah.love@columbia.edu>
2020-07-12 15:01:28 -0600

    Created a window using JFrame and initialized the game in the window.

--- Let's Build A Game/src/com/firstgame/main/Game.java
+++ Let's Build A Game/src/com/firstgame/main/Game.java
@@ -1,9 +1,29 @@
 package com.firstgame.main;
 
-public class Game {
+import java.awt.Canvas;
 
-	public static void main(String args[]) {
+public class Game extends Canvas implements Runnable{
+	
+
+	private static final long serialVersionUID = -6494256809522819072L;
+
+	public static final int WIDTH = 1920, HEIGHT = 1200;
+	
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Let's build a Game!", this);
+	}
+	
+	public synchronized void start() {
+				
+	}
+
+	public void run() {
 		
+					
+	}
+	
+	public static void main(String args[]) {
+		new Game();
 	}
 	
 }
--- Let's Build A Game/src/com/firstgame/main/Window.java
+++ Let's Build A Game/src/com/firstgame/main/Window.java
@@ -1,7 +1,34 @@
 package com.firstgame.main;
 
-public class Window {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-	//testing a comment
+import javax.swing.JFrame;
+
+public class Window extends Canvas{
+
+	/**
+	 * Teh purpose of this class is just to create the window. So we will need a width, height and title.
+	 */
+	private static final long serialVersionUID = -1366450581141666846L;
+	
+	public Window(int width, int height, String title, Game game){
+		JFrame frame = new JFrame(title);  //created a JFrame, the window of our game.  Frame.something means referring back to the jframe. 
+		
+		//create the dimensions of the window		
+		
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));	
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		//functions of the window
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows the window to actual stop the thread
+		frame.setResizable(false); //window cannot be resized
+		frame.setLocationRelativeTo(null); //not needed but now the window starts in the window
+		frame.add(game); //this actually puts the game in the window
+		frame.setVisible(true); //makes you be able to see it
+		game.start(); // something from the game class that we are creating. We are now running the start method.
+	}
 	
 }
