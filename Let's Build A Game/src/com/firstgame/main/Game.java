195e5cc889db40ccbe364ae44fbe2e61de4fe7a0
noahlove2658 <noah.love@columbia.edu>
2020-07-12 15:16:51 -0600

    Created the game loop and explanations.

--- Let's Build A Game/src/com/firstgame/main/Game.java
+++ Let's Build A Game/src/com/firstgame/main/Game.java
@@ -33,8 +33,48 @@ public class Game extends Canvas implements Runnable{
 	}
 
 	public void run() {
+		//this is the game loop, that allows us to update the game.
+		//this code is just made by someone else. Don't totally worry about what it is doing.
 		
-					
+		/*
+		 * "lastTime", "now," and "ns" are used to calculate "delta." amountOfTicks is the amount of tics/second, and ns is the amount of nanoseconds/tick.
+		 * When delta is calculated, you have (now-lastTime)/(ns/tick), but now and lastTime  are in nanoseconds, so it has units "tick". 
+		 * We then add this to delta, and keep going.
+		 * Whenever delta+=1, one tick has passed, and we therefore call the command tick() [[[which is explained in the video]]], 
+		 * and reset delta to 0 in the while(delta>=1) loop.
+		 * the if(running) loop updates the window (by rendering again), and increases the frames with 1.
+		 * the if(System.currentTimeMillis()-timer>1000) loop writes out the FPS once per second by checking if 
+		 * the current time is more than 1000 milliseconds (1 second) larger than "timer" was.
+		 * IF so, we update "timer" to be 1 second later (timer+=1000;), and print the amount of frames that have passed, 
+		 * and set frames to 0. Since this event happens once every second, the value "frames" is the frames per second.
+		 * stop() stops the game.
+		 */
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();					
 	}
 	
 	public static void main(String args[]) {
