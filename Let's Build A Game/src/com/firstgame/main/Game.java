9ace8c21dee02f1f1268c9b9598a1fb8967ee5ef
noahlove2658 <57121482+noahlove2658@users.noreply.github.com>
2020-07-12 12:56:53 -0600

    First commit for let's build a game.

---
+++ Let's Build A Game/src/com/firstgame/main/Game.java
@@ -0,0 +1,9 @@
+package com.firstgame.main;
+
+public class Game {
+
+	public static void main(String args[]) {
+		
+	}
+	
+}
---
+++ Let's Build A Game/src/com/firstgame/main/Window.java
@@ -0,0 +1,5 @@
+package com.firstgame.main;
+
+public class Window {
+
+}
