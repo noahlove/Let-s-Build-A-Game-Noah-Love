e308a5f9ecf511702d209f317d05278f1168f1ec
noahlove2658 <noah.love@columbia.edu>
2020-07-12 18:37:26 -0600

    Work on GameObject class and create Player Class

--- Let's Build A Game/src/com/firstgame/main/GameObject.java
+++ Let's Build A Game/src/com/firstgame/main/GameObject.java
@@ -24,6 +24,34 @@ public abstract class GameObject {
 	public abstract void tick(); 
 	public abstract void render(Graphics g);
 	
-	
-	
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setId(ID id) {
+		this.id = id;
+	}
+	public ID getID() {
+		return id;
+	}
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+	public int getVelX() {
+		return velX;
+	}
+	public int getVelY() {
+		return velY;
+	}
 }
---
+++ Let's Build A Game/src/com/firstgame/main/Player.java
@@ -0,0 +1,23 @@
+package com.firstgame.main;
+
+import java.awt.Graphics;
+
+public class Player extends GameObject {
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+		
+		
+		
+		setX(100);
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+
+}
