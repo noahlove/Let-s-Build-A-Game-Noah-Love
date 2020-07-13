b82b135cdf1915257097bae76fe828389704f8b6
noahlove2658 <noah.love@columbia.edu>
2020-07-12 18:44:02 -0600

    Create Handler Class

--- Let's Build A Game/src/com/firstgame/main/Handler.java
+++ Let's Build A Game/src/com/firstgame/main/Handler.java
@@ -1,8 +1,43 @@
 package com.firstgame.main;
 
+import java.awt.Graphics;
+import java.util.LinkedList;
 
 //this class will update and render all of our objects
 
 public class Handler {
+	
+	//create a list of all the objects in the game
+	LinkedList<GameObject> object = new LinkedList<GameObject>();
+	
+	//reiterates for as many objects as we have in the game
+	public void tick() {
+		for(int i = 0; i< object.size(); i++) {
+			//for each object
+			GameObject tempObject = object.get(i);
+			//gets id of which object we are at. It gets the ith object.
+			
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for(int i = 0; i< object.size(); i++){
+			GameObject tempObject = object.get(i);
+			//render all the game objects
+			tempObject.render(g);
+			
+			
+		}
+		
+	}
+	
+	//function to add and remove game objects from the list
 
+	public void addObject(GameObject object) {
+		this.object.add(object);
+	}
+	public void removeObject(GameObject object) {
+		this.object.remove(object);
+	}
 }
